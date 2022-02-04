44ad733d78b2aaf95bacaf8a564217dce0d5f970
YamanAlhalabi <Rainboi256@gmail.com>
2022-02-04 14:00:27 +0300

    reversed bug.

--- Program.cs
+++ Program.cs
@@ -16,51 +16,6 @@ namespace SharpCanvas
             var apreture = 0.1f;
             var distToFocus = 10f;
 
-
-            for (int i = 0; i < args.Length; i++)
-            {
-                switch(args[i])
-                {
-                    case "--samples":
-                        samples = int.Parse(args[i++]);
-                        break;
-                    case "--scene":
-                        switch(args[i++])
-                        {
-                            case "2spheres":
-                                scene = SceneGenerator.GenerateTwoSphere();
-                            break;
-                            case "marbles":
-                                scene = SceneGenerator.GenerateBouncingMarbles();
-                            break;
-                            
-                            default: 
-                                throw new ArgumentException();
-                        }
-                        break;
-                    case "--bounces":
-                        bounces = int.Parse(args[i++]);
-                        break;
-                    case "--width":
-                        width = int.Parse(args[i++]);
-                        break;
-                    case "--segment":
-                        segmentSize = int.Parse(args[i++]);
-                        break;
-                    case "--aspectRatio":
-                        aspectRatio = float.Parse(args[i++]);
-                        break;
-                    case "--apreture":
-                        apreture = float.Parse(args[i++]);
-                        break;
-                    case "--distToFocus":
-                        distToFocus = float.Parse(args[i++]);
-                        break;
-                    default: 
-                        Console.WriteLine($"argument not recognized {args[i]}");
-                        return;
-                }
-            }
             new Renderer(incrementations, width, samples, segmentSize, aspectRatio, bounces, scene).Begin(apreture, distToFocus);
         }
     }
